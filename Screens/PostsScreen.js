a74735f8495e35a065a03c99d6dc05e839d93e9d
Snizhana Mohyl <snizhana.mogyl@gmail.com>
2023-08-30 01:15:35 +0300

    add: user info on posts screen

--- Screens/PostsScreen.js
+++ Screens/PostsScreen.js
@@ -1,5 +1,5 @@
 import { useEffect, useState } from "react";
-import { FlatList, StyleSheet, View } from "react-native";
+import { FlatList, StyleSheet, View, Image, Text } from "react-native";
 
 import { collection, doc, getDocs, onSnapshot } from "firebase/firestore";
 import { db } from "../firebase/config";
@@ -10,6 +10,8 @@ import { useDispatch, useSelector } from "react-redux";
 export default PostsScreen = ({ route }) => {
   const [posts, setPosts] = useState([]);
   console.log("🚀 ~ file: bhPostsScreen.js:12 ~ posts:", posts);
+
+  const { name, email } = useSelector((state) => state.auth.user);
 
   const getAllPosts = async () => {
     try {
@@ -48,6 +50,18 @@ export default PostsScreen = ({ route }) => {
 
   return (
     <View style={styles.container}>
+      <View style={styles.userWrap}>
+        <Image
+          style={styles.avatar}
+          source={{
+            uri: "https://images.squarespace-cdn.com/content/v1/54b7b93ce4b0a3e130d5d232/1519987020970-8IQ7F6Z61LLBCX85A65S/icon.png?format=1000w",
+          }}
+        />
+        <View style={styles.userInfo}>
+          <Text style={styles.name}>{name}</Text>
+          <Text style={styles.email}>{email}</Text>
+        </View>
+      </View>
       {posts && posts.length > 0 && (
         <FlatList
           style={styles.list}
@@ -65,9 +79,32 @@ export default PostsScreen = ({ route }) => {
 
 const styles = StyleSheet.create({
   container: {
+    paddingTop: 32,
     paddingHorizontal: 16,
     backgroundColor: "#ffffff",
     flex: 1,
+  },
+  userWrap: {
+    flexDirection: "row",
+    gap: 8,
+    alignItems: "center",
+    marginBottom: 32,
+  },
+  avatar: {
+    borderRadius: 16,
+    width: 60,
+    height: 60,
+    overflow: "hidden",
+  },
+  name: {
+    color: "#212121",
+    fontFamily: "Roboto-Bold",
+    fontSize: 13,
+  },
+  email: {
+    color: "rgba(33, 33, 33, 0.80)",
+    fontFamily: "Roboto-Regular",
+    fontSize: 11,
   },
   list: {
     flexDirection: "column",
